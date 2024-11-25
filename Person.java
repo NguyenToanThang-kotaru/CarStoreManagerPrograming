33c915806bb1a76b1fd2ed9f6d056ba43899535d
Alden263 <trungmai12121970@gmail.com>
2024-11-25 15:51:31 +0700

    Add files via upload

--- Person.java
+++ Person.java
@@ -18,28 +18,28 @@ public class Person {
         this.phone=phone;
         this.age=age;
     }
-    public String getname(){
+    public String getName(){
         return name;
     }
-    public void setname(String name){
+    public void setName(String name){
         this.name=name;
     }
-    public String getaddress(){
+    public String getAddress(){
         return address;
     }
-    public void setaddress(String address){
+    public void setAddress(String address){
         this.address=address;
     }
-    public String getphone(){
+    public String getPhone(){
         return phone;
     }
-    public void setphone(String phone){
+    public void setPhone(String phone){
         this.phone=phone;
     }
-    public Short getage(){
+    public Short getAge(){
         return age;
     }
-    public void setage(Short age){
+    public void setAge(short age){
         this.age=age;
     }
     public void input(){
@@ -55,10 +55,10 @@ public class Person {
         
     }
     public void display(){
-        System.out.println("Tên: "+getname());
-        System.out.println("Địa chỉ: "+getaddress());
-        System.out.println("Số điện thoại: "+getphone());
-        System.out.println("Tuổi: "+getage());
+        System.out.println("Tên: "+name);
+        System.out.println("Địa chỉ: "+address);
+        System.out.println("Số điện thoại: "+phone);
+        System.out.println("Tuổi: "+age);
     }
     @Override
     public String toString() {
