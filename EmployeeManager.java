67376e669f20a3f9516b4bc13fab17c15b2f6cbf
ALDEN <trungmai12121970@gmail.com>
2024-12-02 18:41:59 +0700

    Sửa một chút

--- EmployeeManager.java
+++ EmployeeManager.java
@@ -4,16 +4,16 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 
-
-public class EmployeeManager{
-    Scanner scanner=new Scanner(System.in);
+public class EmployeeManager {
+    Scanner scanner = new Scanner(System.in);
     private EmployeeList1 shopEmployee;
-    public EmployeeManager(){
-        shopEmployee= new EmployeeList1();
+
+    public EmployeeManager() {
+        shopEmployee = new EmployeeList1();
         this.readFromFile();
-       
 
     }
+
     public void readFromFile("./employee.txt"){
         try {
             BufferedReader input = new BufferedReader(new FileReader("./database/employee.txt"));
@@ -30,7 +30,7 @@ public class EmployeeManager{
             ex.printStackTrace();
         }            
     }
-    
+
     public void writeToFile("./employee.txt"){
         Employee[] employees = shopEmployee.getEmployees();
         try {
@@ -44,46 +44,47 @@ public class EmployeeManager{
             System.out.println(e);
         }
     }
+
     public void displayMenu() {
-        boolean exit=false;
-        do{
+        boolean exit = false;
+        do {
             HeaderFooter.printHeader("Menu");
-            System.out.println("1: Xem sản phẩm");
-            System.out.println("2: Thêm sản phẩm");
-            System.out.println("3: Sửa thông tin sản phẩm");
-            System.out.println("4: Xoá sản phẩm");
-            System.out.println("5: Tìm sản phẩm");
+            System.out.println("1: Xem nhân viên");
+            System.out.println("2: Thêm nhân viên");
+            System.out.println("3: Sửa thông tin nhân viên");
+            System.out.println("4: Xoá nhân viên");
+            System.out.println("5: Tìm nhân viên");
             System.out.println("exit: Thoát");
             String luachon = "";
             System.out.print("Nhập lựa chọn: ");
-            luachon = sc.nextLine();
+            luachon = scanner.nextLine();
             switch (luachon) {
                 case "1":
-                    HeaderFooter.printHeader("Thông tin của các sản phẩm");
-                    shopProducts.display();
+                    HeaderFooter.printHeader("Thông tin của các nhân viên");
+                    shopEmployee.display();
                     HeaderFooter.printFooter();
                     break;
                 case "2":
-                    HeaderFooter.printHeader("Thêm sản phẩm");
-                    shopProducts.add();
+                    HeaderFooter.printHeader("Thêm nhân viên");
+                    shopEmployee.add();
                     HeaderFooter.printFooter();
                     break;
                 case "3":
-                    HeaderFooter.printHeader("Sửa sản phẩm");
-                    shopProducts.edit();
+                    HeaderFooter.printHeader("Sửa nhân viên");
+                    shopEmployee.edit();
                     HeaderFooter.printFooter();
                     break;
                 case "4":
-                    HeaderFooter.printHeader("Xoá sản phẩm");
-                    shopProducts.delete();
+                    HeaderFooter.printHeader("Xoá nhân viên");
+                    shopEmployee.delete();
                     HeaderFooter.printFooter();
                     break;
                 case "5":
-                    HeaderFooter.printHeader("Tìm kiếm sản phẩm");
-                    Product product = shopProducts.search();
-                    if(product != null){
-                        product.display();
-                    } else{
+                    HeaderFooter.printHeader("Tìm kiếm nhân viên");
+                    Employee employee = shopEmployee.search();
+                    if (employee != null) {
+                        employee.display();
+                    } else {
                         System.out.println("Không tìm thấy sản phẩm!!!");
                     }
                     HeaderFooter.printFooter();
@@ -97,8 +98,7 @@ public class EmployeeManager{
                     System.out.println("Nếu muốn thoát vui lòng nhập \"exit\"");
                     break;
             }
-        } while(!exit);
+        } while (!exit);
     }
 
-
-    
+}
